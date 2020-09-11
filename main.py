28148c7cc53c9716d5b82f998ac9aff601155e98
Jörg Reisslein <joerg@reisslein.de>
2020-09-11 16:12:04 +0200

    110920

--- main.py
+++ main.py
@@ -1,12 +1,20 @@
 import random
 
+#Lösung mit Schleife
 liste = []
-
 i = 0
 while i < 6:
     zufallszahl = (random.randint(1,49))
     if zufallszahl not in liste:
         liste.append(zufallszahl)
         i += 1
+liste.sort()
+print(liste)
+
+#Alternative ohne Schleife
+liste = []
+zahlenraum = []
+zahlenraum.extend(range(1, 49))
+liste = random.sample(zahlenraum, 6)
 liste.sort()
 print(liste)
